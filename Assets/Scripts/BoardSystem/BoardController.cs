7a2588e34906e3365f47f5f28e249d4231388627
pumbas600 <joshjeffers600@gmail.com>
2021-04-03 15:22:00 +1300

    Created simple board generator
    Still TODO:
    - Controlling piece positions and ability to easily determine if there's a piece at a specific position

--- Assets/Scripts/BoardSystem/BoardController.cs
+++ Assets/Scripts/BoardSystem/BoardController.cs
@@ -1,16 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class BoardController : MonoBehaviour {
+public class BoardController : Singleton<BoardController>
+{
+	[SerializeField] private Vector2Int boardSize;
+	
+	[SerializeField] private Tilemap boardTilemap;
+	[SerializeField] private Tile tileA;
+	[SerializeField] private Tile tileB;
+	
+	public Vector2Int BoardSize
+	{
+		get
+		{
+			return boardSize;
+		}
+	}
 
+	[InspectorButton]
+	private void UpdateBoard()
+	{
+		if (tileA == null || tileB == null || boardTilemap == null)
+		{
+			Debug.LogWarning("Make sure all the necessary assets are set in the inspector before trying to update the board");
+			return;
+		}
+
+		//The board is already the correct size
+		if (boardTilemap.size.x == boardSize.x && boardTilemap.size.y == boardSize.y)
+			return;
+
+		//Clear the previous board
+		boardTilemap.ClearAllTiles();
+		Vector2Int bottomLeftCorner = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+
+		for (int x = 0; x < boardSize.x; x++)
+		{
+			for (int y = 0; y < boardSize.y; y++)
+			{
+				Vector3Int tilePosition = new Vector3Int(bottomLeftCorner.x + x, bottomLeftCorner.y + y, 0);
+				Tile tile = DetermineTile(x, y);
+				
+				boardTilemap.SetTile(tilePosition, tile);
+			}
+		}
+		boardTilemap.ResizeBounds();
+	}
+
+	private Tile DetermineTile(int x, int y)
+	{
+		return y % 2 == x % 2
+			? tileA
+			: tileB;
+	}
+	
 	// Use this for initialization
-	void Start () {
-		
+	private void Start ()
+	{
+		UpdateBoard();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		
 	}
 }
---
+++ Assets/Scripts/Utilities/Editors/MonoBehaviourEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+[CustomEditor(typeof(MonoBehaviour), true)]
+public class MonobehaviourInspectorButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (!Application.isEditor) return;
+        
+        var type = target.GetType();
+
+        foreach (var method in type.GetMethods(
+            BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+        {
+            var attributes = method.GetCustomAttributes(typeof(InspectorButtonAttribute), true);
+
+            if (attributes.Length != 0)
+            {
+                if (GUILayout.Button("Run: " + method.Name))
+                {
+                    //If the user clicks the button, call the method
+                    method.Invoke(target, new object[] { });
+                }
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
