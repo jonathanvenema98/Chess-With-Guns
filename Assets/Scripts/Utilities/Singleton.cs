8a0706237cf9356c6d6a3bc104b8ebd7dcd141a6
pumbas600 <joshjeffers600@gmail.com>
2021-04-03 13:08:11 +1300

    Added Singleton class, created folders for the different main systems

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T: Singleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+                CreateInstance();
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = (T)this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private static void CreateInstance()
+    {
+        var gameObject = new GameObject
+        {
+            name = typeof(T).Name
+        };
+        gameObject.AddComponent<T>();
+    }
+}
