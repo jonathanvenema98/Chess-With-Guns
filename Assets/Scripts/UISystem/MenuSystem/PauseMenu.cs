b8256b22893dc6fb1003263f5ee40998d214e501
grammar15016 <81835682+grammar15016@users.noreply.github.com>
2021-04-05 16:20:57 +1200

    Added StateMachine pause(?)
    Also corrected line 43

--- Assets/Scripts/UISystem/MenuSystem/PauseMenu.cs
+++ Assets/Scripts/UISystem/MenuSystem/PauseMenu.cs
@@ -3,44 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseMenu : MonoBehaviour
+{
 
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape))
+
+    private State previousState;
+
+    public class PausedState : State
+    {
+        public override void OnStart()
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
                 Resume();
-            } else
+            }
+            else
             {
                 Pause();
             }
         }
-	}
+    }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+        //Time.timeScale = 1f;
         GameIsPaused = false;
+        StateMachine.Instance.SetState(previousState);
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        //Time.timeScale = 0f;
         GameIsPaused = true;
+        previousState = StateMachine.Instance.State;
+        StateMachine.Instance.SetState(new PausedState());
     }
 
     public void LoadMenu()
     {
         Debug.Log("Loading menu...");
-        Time.timeScale = 1f;
+        Resume();
         SceneManager.LoadScene("Menu");
     }
 
